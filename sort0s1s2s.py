e9d3cba411176f519586b9f1ee57ac4e1b082434
sahil <sahil13399@gmail.com>
2020-10-11 14:51:09 +0530

    Sort012 11/10

---
+++ sort0s1s2s.py
@@ -0,0 +1,21 @@
+class Solution:
+            
+    def sortColors(self, nums: List[int]) -> None:
+        
+        n=len(nums)
+        
+        low=0;mid=0;high=n-1
+        
+        while(mid<=high):
+            
+            if nums[mid]==1:
+                mid+=1
+            elif nums[mid]<1:
+                nums[mid],nums[low]=nums[low],nums[mid]
+                mid+=1
+                low+=1
+            else:
+                nums[mid],nums[high]=nums[high],nums[mid]
+                high-=1
+                
+        return
